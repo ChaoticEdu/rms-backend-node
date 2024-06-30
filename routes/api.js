4c8ca4605124ce6b83764c239fcd9c7086ea3f8b
ChaoticEdu <prabesh.gurung@apexcollege.edu.np>
2024-06-30 15:58:45 +0545

    routes added

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,13 @@ var restaurantroute = require('./api/restaurant');
 var menuroute = require('./api/menu')
 var userroute = require('./api/user');
 var tableroute = require('./api/table');
+var orderroute = require('./api/order')
+var billroute = require('./api/bill');
 
 router.use('/restaurant',restaurantroute);
 router.use('/menu',menuroute);
 router.use('/user',userroute);
 router.use('/table',tableroute);
+router.use('/order',orderroute);
 
 module.exports = router;
